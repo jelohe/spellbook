24a885d5f76643d3dab983a2ef935e37bcf9f67a
jelohe <jsus.90@gmail.com>
2024-11-05 18:22:52 +0100

    Extract create spell component to make it reusable

--- lib/spellbook_web/live/create_spell_live.ex
+++ lib/spellbook_web/live/create_spell_live.ex
@@ -1,31 +1,8 @@
 defmodule SpellbookWeb.CreateSpellLive do
   use SpellbookWeb, :live_view
-  alias Spellbook.{Spells, Spells.Spell}
+  alias SpellbookWeb.SpellFormComponent
 
   def mount(_params, _session, socket) do
-    empty_spell = Spells.change_spell(%Spell{})
-    new_socket = assign(socket, form: to_form(empty_spell))
-
-    {:ok, new_socket}
-  end
-
-  def handle_event("validate", %{"spell" => params}, socket) do
-    changeset = %Spell{}
-      |> Spells.change_spell(params)
-      |> Map.put(:action, :validate)
-    {:noreply, assign(socket, :form, to_form(changeset))}
-  end
-
-  def handle_event("create", %{"spell" => params}, socket) do
-    case Spells.create_spell(socket.assigns.current_user, params) do
-      {:ok, spell} ->
-        socket = push_event(socket, "spell-created", %{})
-        changeset = Spells.change_spell(%Spell{})
-        socket = assign(socket, :form, to_form(changeset))
-        {:noreply, push_navigate(socket, to: ~p"/spell?#{[id: spell]}")}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :form, to_form(changeset))}
-    end
+    {:ok, socket}
   end
 end
---
+++ lib/spellbook_web/live/spell_form_component.ex
@@ -0,0 +1,82 @@
+defmodule SpellbookWeb.SpellFormComponent do
+  use SpellbookWeb, :live_component
+  alias Spellbook.{Spells, Spells.Spell}
+
+  def mount(socket) do
+    empty_spell = Spells.change_spell(%Spell{})
+    new_socket = assign(socket, form: to_form(empty_spell))
+
+    {:ok, new_socket}
+  end
+  
+  def handle_event("validate", %{"spell" => params}, socket) do
+    changeset = %Spell{}
+      |> Spells.change_spell(params)
+      |> Map.put(:action, :validate)
+    {:noreply, assign(socket, :form, to_form(changeset))}
+  end
+
+  def handle_event("create", %{"spell" => params}, socket) do
+    case Spells.create_spell(socket.assigns.current_user, params) do
+      {:ok, spell} ->
+        socket = push_event(socket, "spell-created", %{})
+        changeset = Spells.change_spell(%Spell{})
+        socket = assign(socket, :form, to_form(changeset))
+        {:noreply, push_navigate(socket, to: ~p"/spell?#{[id: spell]}")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :form, to_form(changeset))}
+    end
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.form for={@form} phx-submit="create" phx-change="validate" phx-target={@myself}>
+        <div class="justify center px-28 w-full space-y-4 mb-10">
+          <.input
+            field={@form[:description]}
+            placeholder="Spell description..."
+            autocomplete="off"
+            phx-debounce="blur"
+          />
+          <div>
+            <div class="flex p-2 items-center bg-spDark rounded-t-md border border-b-0">
+              <div class="w-[300px] mb-2">
+                <.input
+                  field={@form[:name]}
+                  placeholder="Filename including extension..."
+                  autocomplete="off"
+                  phx-debounce="blur"
+                />
+              </div> 
+             </div>
+             <div id="spell-wrapper" class="flex w-full" phx-update="ignore">
+               <textarea
+                 id="line-numbers"
+                 class="line-numbers"
+                 readonly
+               ><%= "1\n" %></textarea>
+               <div class="w-full">
+                 <.input
+                   id="spell-textarea"
+                   phx-hook="UpdateLineNumber"
+                   field={@form[:markup_text]}
+                   type="textarea"
+                   placeholder="Write your spell"
+                   spellcheck="false"
+                   autocomplete="off"
+                   phx-debounce="blur"
+                 />
+               </div>
+            </div>
+          </div>
+          <div class="flex justify-end">
+            <.button class="create_button" phx-disable-with="Creating...">Create Spell</.button>
+          </div>
+        </div>
+      </.form>
+    </div>
+    """
+  end
+end
